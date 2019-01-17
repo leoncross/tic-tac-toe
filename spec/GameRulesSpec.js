96de27fcd14ae92839ff00e1d2f3fcd4bbafc983
leoncross <leon@lscross.co.uk>
2019-01-17 16:34:28 +0000

    formating improvement

--- spec/GameRulesSpec.js
+++ spec/GameRulesSpec.js
@@ -11,23 +11,23 @@ describe('GameRules', function() {
   var winDiagO = [{ row: 1, column: 2, score: 1 }, { row: 1, column: 3, score: -1 }, { row: 1, column: 1, score: 1 }, { row: 2, column: 2, score: -1 }, { row: 2, column: 1, score: 1 }, { row: 3, column: 1, score: -1 }]
 
 
-describe("#_checkWinner", function() {
-  it("win found 'x')", function() {
-    expect(gameRules.check(winnerX)).toEqual(true)
+  describe("#_checkWinner", function() {
+    it("win found 'x')", function() {
+      expect(gameRules.check(winnerX)).toEqual(true)
+    })
+    it("win found 'o')", function() {
+      expect(gameRules.check(winnerO)).toEqual(true)
+    })
+    it("no win", function() {
+      expect(gameRules.check(noWin)).toEqual(false)
+    })
   })
-  it("win found 'o')", function() {
-    expect(gameRules.check(winnerO)).toEqual(true)
+  describe("#_checkWinnerDiagonal", function() {
+    it("win in diagonal 1 'x'", function() {
+      expect(gameRules.check(winDiagX)).toEqual(true)
+    })
+    it("win in diagonal 2 'o'", function() {
+      expect(gameRules.check(winDiagO)).toEqual(true)
+    })
   })
-  it("no win", function() {
-    expect(gameRules.check(noWin)).toEqual(false)
-  })
-})
-describe("#_checkWinnerDiagonal", function() {
-  it("win in diagonal 1 'x'", function() {
-    expect(gameRules.check(winDiagX)).toEqual(true)
-  })
-  it("win in diagonal 2 'o'", function() {
-    expect(gameRules.check(winDiagO)).toEqual(true)
-  })
-})
 });
